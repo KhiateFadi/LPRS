5a23695c5565c3f2215f3dff7e523daf5a195e84
iliassalmi <56584061+iliassalmi@users.noreply.github.com>
2020-09-28 14:32:04 +0200

    REDIRECTION INSCRIPTION

--- main/HTML/manager/manager.php
+++ main/HTML/manager/manager.php
@@ -31,17 +31,17 @@ session_start();
 
           $req = $bdd->prepare('INSERT INTO utilisateur(nom, prenom, mdp, mail) VALUES(:nom, :prenom, :mdp, :mail)');
           $a = $req->execute(array('nom'=>$new->getNom(), 'prenom'=>$new->getPrenom(), 'mdp'=>md5($new->getMdp()), 'mail'=>$new->getMail()));
-          $b = $red->fetch();
+          $b = $req->fetch();
 if ($b == true) {
             $_SESSION['id'] = $b['id'];
 
-            header('Location: ../index.html');
+            header('Location: ../../index.html');
 
 
           }
           else {
             echo "Mauvais login veuillez réessayer !";
-            header('Location:../index.html');
+            header('Location:../indexx.html');
           }
     }
 

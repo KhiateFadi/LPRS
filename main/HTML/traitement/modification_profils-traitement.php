1be5269bd31e4785cd562fc5184ee318e7fa153b
KhiateFadi <56584146+KhiateFadi@users.noreply.github.com>
2020-10-05 17:23:25 +0200

    test

--- main/HTML/traitement/modification_profils-traitement.php
+++ main/HTML/traitement/modification_profils-traitement.php
@@ -2,8 +2,7 @@
 
 require_once('../modele/modification_profils_class.php');
 require_once('../manager/manager.php');
-var_dump($_POST);
-die();
+
 $modification_profils_class = new modifier_profils_class($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['mdp']);
 $manager = new Manager();
 $manager->modification_user($modification_profils_class);

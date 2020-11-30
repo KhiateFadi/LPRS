b5ba39351f0eef7fbd2809ca09bf725521d34544
iliassalmi <56584061+iliassalmi@users.noreply.github.com>
2020-11-30 13:54:22 +0100

    Revert "php mailer fonctionnelle" This reverts commit ca43908956502e1dd449ed1b6bbc24e21412b84b.

--- main/HTML/manager/manager.php
+++ main/HTML/manager/manager.php
@@ -37,14 +37,25 @@ public function connexion($con){
           $req = $bdd->prepare('INSERT INTO utilisateur(nom, prenom, mdp, mail) VALUES(:nom, :prenom, :mdp, :mail)');
           $a = $req->execute(array('nom'=>$new->getNom(), 'prenom'=>$new->getPrenom(), 'mdp'=>md5($new->getMdp()), 'mail'=>$new->getMail()));
           $b = $req->fetch();
+if ($b == true) {
+            $_SESSION['id'] = $b['id'];
+            header('Location: ../../../index.php');
+            mail($new->getMail());
 
 
+          }
+          else {
+           echo "Mauvais login veuillez réessayer !";
+           header('Location:../../../index.php');
+          }
+}
+          public function Mail($donnee){
           $mail = new PHPMailer();
           $mail->isSMTP();                                            // Send using SMTP
           $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
           $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-          $mail->Username   = 'irisitalianna75@gmail.com';                     // SMTP username
-          $mail->Password   = 'iris75000';                               // SMTP password
+          $mail->Username   = 'iliassalmiii@outlook.com';                     // SMTP username
+          $mail->Password   = '4d2d909e';                               // SMTP password
           $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
           $mail->Port       = 587;                                    // TCP port to connect to
 
@@ -56,10 +67,10 @@ public function connexion($con){
             echo '<body onLoad="alert(\'Erreur\')">';
           echo '<meta http-equiv="refresh" content="0;URL=../View/contact.php">';
           } else {
-             header("location: ../../../index.php");
+             header("location: ../../index.php");
           }
-}
 
+    }
 
 public function mdp($new){
         $bdd = new PDO('mysql:host=localhost;dbname=lprs;charset=utf8','root','');
